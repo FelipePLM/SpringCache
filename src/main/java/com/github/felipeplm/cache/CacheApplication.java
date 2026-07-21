4a05abc0c4e41237093f187336ba941cb8747774
LuisFPLima <luislima@plurismidia.com.br>
2026-07-21 17:19:13 -0300

    Caching with Redis for multiple instances

--- src/main/java/com/github/felipeplm/cache/CacheApplication.java
+++ src/main/java/com/github/felipeplm/cache/CacheApplication.java
@@ -1,15 +1,14 @@
 package com.github.felipeplm.cache;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +18,6 @@ public class CacheApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class, args);
-    }
-
-    @Bean
-    CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("products");
     }
 
     @Bean
@@ -40,7 +34,7 @@ public class CacheApplication {
     }
 }
 
-record Product(Long id, String name, String description) {
+record Product(Long id, String name, String description) implements Serializable {
 }
 
 @Service
