fc31a07fded78bdac97133cc829bcf72a0a1382e
LuisFPLima <luislima@plurismidia.com.br>
2026-07-21 16:45:42 -0300

    In-instance caching

--- src/main/java/com/github/felipeplm/cache/CacheApplication.java
+++ src/main/java/com/github/felipeplm/cache/CacheApplication.java
@@ -6,29 +6,39 @@ import java.util.Map;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@EnableCaching
 public class CacheApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class, args);
     }
 
-	@Bean
-	ApplicationRunner runner(ProductService productService) {
-		return args -> {
-			System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-			System.out.println("Id: 1: " + productService.getById(1L));
-			System.out.println("Id: 2: " + productService.getById(2L));
-			System.out.println("Id: 1: " + productService.getById(1L));
-			System.out.println("Id: 1: " + productService.getById(1L));
-			System.out.println("Id: 1: " + productService.getById(1L));
-		};
-	}
-}
+    @Bean
+    CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("products");
+    }
 
+    @Bean
+    ApplicationRunner runner(ProductService productService) {
+        return args -> {
+            System.out.println("\n\n\n\n");
+
+            System.out.println("Id 1: " + productService.getById(1L));
+            System.out.println("Id 2: " + productService.getById(2L));
+            System.out.println("Id 1: " + productService.getById(1L));
+            System.out.println("Id 1: " + productService.getById(1L));
+            System.out.println("Id 1: " + productService.getById(1L));
+        };
+    }
+}
 
 record Product(Long id, String name, String description) {
 }
@@ -46,18 +56,21 @@ class ProductService {
         products.put(5L, new Product(5L, "SSD", "SSD NVMe 1 TB"));
     }
 
-    Product getById(Long id) {
-        System.out.println("Searching product...");
-		simulateLatency();
+    @Cacheable(value = "products", key = "#id")
+    public Product getById(Long id) {
+        System.out.println("Searching product ID " + id + "...");
+
+        simulateLatency();
+
         return products.get(id);
     }
 
-	private void simulateLatency() {
-		try {
-			long time = 1000L;
-			Thread.sleep(time);
-		} catch (InterruptedException e) {
-			throw new IllegalStateException(e);
-		}
-	}
+    private void simulateLatency() {
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException exception) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException(exception);
+        }
+    }
 }
